c17675f3d2646ad4753f3a7a392fd5aa84137b95
vincedacoolkid@gmail.com <vincedacoolkid@gmail.com@74868f00-8f8a-6c55-320a-a8e749a92dc2>
2013-02-27 19:20:13 +0000

    Added console outputs to almost all functions. added a save settings method, this is where the database will be needed. console exports to a text file. ~thanks matt

--- trunk/Dresscode/trunk/Dresscode/Email.cs
+++ trunk/Dresscode/trunk/Dresscode/Email.cs
@@ -17,6 +17,8 @@ namespace Dresscode
         }
         //globals
         bool editmode = false;
+        System.IO.StreamWriter SR;
+        //textBox_console.Text += "\r\n";
         //
 
         private void button_add_email_Click(object sender, EventArgs e)
@@ -29,6 +31,7 @@ namespace Dresscode
                     if (listBox_emails.Items[i].ToString() == textBox_add_email.Text)
                     {
                         MessageBox.Show("That email adress is already on the send list");
+                        textBox_console.Text += "That email adress is already on the send list \r\n";
                         exists = true;
                         break;
                     }
@@ -36,6 +39,7 @@ namespace Dresscode
                 if (!exists)
                 {
                     listBox_emails.Items.Add(textBox_add_email.Text);
+                    textBox_console.Text += "Added Email: " + textBox_add_email.Text +" \r\n";
                     textBox_add_email.Clear();
                 }
             }
@@ -43,6 +47,7 @@ namespace Dresscode
 
         private void Email_Load(object sender, EventArgs e)
         {
+            textBox_console.Text += "Initializing Settings from database.\r\n";
             // pull old settings from database and set them here
                 //hours for send time
                 //minutes for send time
@@ -55,12 +60,14 @@ namespace Dresscode
         private void button_start_Click(object sender, EventArgs e)
         {
             //http://msdn.microsoft.com/en-us/library/system.net.mail.smtpclient.aspx
+            textBox_console.Text += "Starting task\r\n";
         }
 
         private void button_remove_email_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show("Do you want to delete " + listBox_emails.SelectedItem.ToString() + "?", "Really Delete Email?", MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
+                textBox_console.Text += "Removed: "+ listBox_emails.SelectedItem +".\r\n";
                 listBox_emails.Items.Remove(listBox_emails.SelectedItem);
             }
         }
@@ -69,6 +76,7 @@ namespace Dresscode
         {
             if (!editmode)
             {
+                textBox_console.Text += "Edit Mode Active\r\n";
                 editmode = true;
                 button_edit_settings.Text = "Done";
                 numericUpDown_hours.Enabled = true;
@@ -79,7 +87,8 @@ namespace Dresscode
             }
             else
             {
-                //add a method here that saves the changes to the database
+                saveSettings();
+                textBox_console.Text += "Edit Mode Disabled\r\n";
                 editmode = false;
                 button_edit_settings.Text = "Edit Settings";
                 numericUpDown_hours.Enabled = false;
@@ -87,6 +96,44 @@ namespace Dresscode
                 textBox_host_email.Enabled = false;
                 textBox_email_subject.Enabled = false;
                 numericUpDown_port.Enabled = false;
+            }
+        }
+
+        public void saveSettings()
+        {
+            //send new settings to database here.
+            textBox_console.Text += "Send time set to "+ numericUpDown_hours.Value.ToString() +":"+ numericUpDown_minutes.Value.ToString() +"\r\n";
+            textBox_console.Text += "Host email set to "+ textBox_host_email.Text +"\r\n";
+            textBox_console.Text += "Email subject set to "+ textBox_email_subject.Text +"\r\n";
+            textBox_console.Text += "Host email port set to "+ numericUpDown_port.Value.ToString() +"\r\n";
+            textBox_console.Text += "Settings saved to database\r\n";
+        }
+
+        private void button_clear_console_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Really clear console?","Clear",MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                textBox_console.Clear();
+                textBox_console.Text += "Console Cleared.\r\n";
+            }
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            //stop
+            textBox_console.Text += "Stopping task\r\n";
+        }
+
+        private void button_save_console_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text document | *.txt";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                SR = new System.IO.StreamWriter(sfd.FileName);
+                SR.WriteLine(textBox_console.Text);
+                SR.Flush();
+                SR.Close();
             }
         }
     }
